635157ca4e56a3a9210b7fbb133535b6be046e5b
iniya1222007 -code <iniya1222007@gmail.com>
2026-01-30 16:34:59 +0530

    video commit

--- accident-deduction-rescue/ui_alerts.py
+++ accident-deduction-rescue/ui_alerts.py
@@ -3,8 +3,10 @@ import streamlit as st
 def render_controls():
     st.sidebar.header("🎮 Simulation Controls")
 
-    speed = st.sidebar.slider("Ambulance Speed", 1, 10, 5)
-    impact = st.sidebar.slider("Impact Intensity", 1, 10, 5)
+    speed = st.sidebar.slider("Ambulance Speed", 1, 10, 5, key="speed_slider")
+    impact = st.sidebar.slider("Impact Intensity", 1, 10, 5, key="impact_slider")
+
+
 
     accident_detected = impact >= 7
 
@@ -15,6 +17,3 @@ def render_controls():
 
     return speed, impact, accident_detected
 
-# just 2 simple lines to run it
-st.title("Accident Simulation")
-render_controls()
